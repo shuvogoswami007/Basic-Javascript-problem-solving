66e368ae2c57543f590225341a5886a42167a4a7
Shuvo Goswami <shuvogoswami007@gmail.com>
2021-01-22 03:58:05 -0800

    Added

--- assignment.js
+++ assignment.js
@@ -1,7 +1,19 @@
+function kilometerToMeter(kilo){
+    var meter = kilo * 1000;
+
+    if(meter < 0){
+        console.log("undefined");
+    }
+    else {
+        return meter;
+    }
+}
+    var result = kilometerToMeter(-15);
+    console.log(result);
 
 
-
-function budgetCalculator(ram, sam, jodu){
+function budgetCalculator(ram, sam, jodu) //ram = clock , sam = mobile , jodu = laptop//
+{
     var clock = 50 * ram;
     var mobile = 100 * sam;
     var laptop = 500 * jodu;
