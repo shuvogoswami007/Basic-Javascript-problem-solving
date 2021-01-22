5aa4c9d366dcb0330a87394eb48ffb1a4875c403
Shuvo Goswami <shuvogoswami007@gmail.com>
2021-01-22 03:16:53 -0800

    First part

--- assignment.js
+++ assignment.js
@@ -1,3 +1,12 @@
-function kilometerToMeter(){
-    
+function kilometerToMeter(kilo){
+    var meter = kilo * 1000;
+
+    if(meter < 0){
+        console.log("undefined");
+    }
+    else {
+        return meter;
+    }
 }
+    var result = kilometerToMeter(-15);
+    console.log(result);
